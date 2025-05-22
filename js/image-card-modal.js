380a21e68a148a54e15b64488aedf4f2aea7d6a0
Ifeouwa Olujobi <ifeoluwaolujobi@gmail.com>
2025-05-22 11:34:58 +0100

    Modal for image card details

--- js/image-card-modal.js
+++ js/image-card-modal.js
@@ -2,24 +2,53 @@ export function imageCardModal() {
   const imgContainer = document.querySelector(".images");
   const imgDetails = document.querySelector(".modal");
   const cardOverlay = document.querySelector(".modal-overlay");
-  let imgOverview;
+
   //USING EVENT DELEGATION
   imgContainer.addEventListener("click", (e) => {
     const image = e.target.closest("img");
-    imgOverview = image;
 
     if (!image) return;
-    console.log("welcome to my modal");
-    console.log(image.src);
-    console.log(image.alt);
-
     imgDetails.classList.remove("modal-hidden");
     cardOverlay.classList.remove("modal-hidden");
 
-    const html = `
+    let html = `
+    <div class='modal-container'>
       <img src ='${image.src}' alt='${image.alt}' class='modal-img'/>
       <p>${image.alt}</p>
+      <button class='close-modal-btn'>CLOSE</button>
+    </div>
     `;
     imgDetails.insertAdjacentHTML("afterbegin", html);
+
+    //CLOSE MODAL
+    const closeBtn = document.querySelector(".close-modal-btn");
+    closeBtn.addEventListener("click", () => {
+      closeModal();
+    });
+
+    cardOverlay.addEventListener("click", (e) => {
+      if (
+        !imgDetails.classList.contains("modal-hidden") &&
+        !cardOverlay.classList.contains("modal-hidden")
+      ) {
+        closeModal();
+      }
+    });
+    document.body.addEventListener("keydown", (e) => {
+      if (
+        e.key === "Escape" &&
+        !imgDetails.classList.contains("modal-hidden") &&
+        !cardOverlay.classList.contains("modal-hidden")
+      ) {
+        closeModal();
+      }
+    });
+
+    function closeModal() {
+      imgDetails.classList.add("modal-hidden");
+      cardOverlay.classList.add("modal-hidden");
+
+      imgDetails.innerHTML = "";
+    }
   });
 }
