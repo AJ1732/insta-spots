19355793eafaa93f861ecc4e6fac7ea4ecedd050
Ifeouwa Olujobi <ifeoluwaolujobi@gmail.com>
2025-05-22 09:54:41 +0100

    Modal displays Image details

--- js/image-card-modal.js
+++ js/image-card-modal.js
@@ -1,3 +1,25 @@
 export function imageCardModal() {
-  console.log("Hello from Image Card Modal");
+  const imgContainer = document.querySelector(".images");
+  const imgDetails = document.querySelector(".modal");
+  const cardOverlay = document.querySelector(".modal-overlay");
+  let imgOverview;
+  //USING EVENT DELEGATION
+  imgContainer.addEventListener("click", (e) => {
+    const image = e.target.closest("img");
+    imgOverview = image;
+
+    if (!image) return;
+    console.log("welcome to my modal");
+    console.log(image.src);
+    console.log(image.alt);
+
+    imgDetails.classList.remove("modal-hidden");
+    cardOverlay.classList.remove("modal-hidden");
+
+    const html = `
+      <img src ='${image.src}' alt='${image.alt}' class='modal-img'/>
+      <p>${image.alt}</p>
+    `;
+    imgDetails.insertAdjacentHTML("afterbegin", html);
+  });
 }
