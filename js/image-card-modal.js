9ef6f7ef1a1ba288d38ee55589c7705339ed734b
Ifeouwa Olujobi <ifeoluwaolujobi@gmail.com>
2025-05-23 09:00:48 +0100

    Image Card Details Interactivity

--- js/image-card-modal.js
+++ js/image-card-modal.js
@@ -1,20 +1,18 @@
 export function imageCardModal() {
   const imgContainer = document.querySelector(".images");
-  const imgDetails = document.querySelector(".modal");
-  const cardOverlay = document.querySelector(".modal-overlay");
+  const imgDetails = document.querySelector("#close-modal");
 
   //USING EVENT DELEGATION
   imgContainer.addEventListener("click", (e) => {
     const image = e.target.closest("img");
 
     if (!image) return;
-    imgDetails.classList.remove("modal-hidden");
-    cardOverlay.classList.remove("modal-hidden");
+    imgDetails.showModal();
 
     let html = `
     <div class='modal-container'>
       <img src ='${image.src}' alt='${image.alt}' class='modal-img'/>
-      <p>${image.alt}</p>
+      <p class='favourite'>${image.alt}</p>
       <button class='close-modal-btn'>CLOSE</button>
     </div>
     `;
@@ -26,28 +24,22 @@ export function imageCardModal() {
       closeModal();
     });
 
-    cardOverlay.addEventListener("click", (e) => {
-      if (
-        !imgDetails.classList.contains("modal-hidden") &&
-        !cardOverlay.classList.contains("modal-hidden")
-      ) {
+    document.body.addEventListener("keydown", (e) => {
+      if (e.key === "Escape") {
         closeModal();
       }
     });
-    document.body.addEventListener("keydown", (e) => {
-      if (
-        e.key === "Escape" &&
-        !imgDetails.classList.contains("modal-hidden") &&
-        !cardOverlay.classList.contains("modal-hidden")
-      ) {
+
+    imgDetails.addEventListener("click", (e) => {
+      const contentBox = document.querySelector(".modal-container");
+
+      if (contentBox && !contentBox.contains(e.target)) {
         closeModal();
       }
     });
 
     function closeModal() {
-      imgDetails.classList.add("modal-hidden");
-      cardOverlay.classList.add("modal-hidden");
-
+      imgDetails.close();
       imgDetails.innerHTML = "";
     }
   });
