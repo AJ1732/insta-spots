e3019b475ec584396221a5062b82ec274d8d3175
Akangbe Ooreoluwa <akangbepattie@gmail.com>
2025-05-23 21:40:22 +0100

    Favourite icon with hover effect

--- js/favourite-icon.js
+++ js/favourite-icon.js
@@ -2,24 +2,25 @@ export function favouriteIcon() {
 
 const favouriteIcons = document.querySelectorAll("#favourite-icon");
 
-console.log("Found icons:", favouriteIcons);
-console.log("Number of icons:", favouriteIcons.length);
 
 favouriteIcons.forEach(icon => {
-  console.log("Adding event listener to icon:", icon);
+  
+  icon.classList.add("favourite-icon");
+
   icon.addEventListener("click", () => {
-    console.log("Icon clicked:", icon);
+    
   
   if (icon.classList.contains("liked")) {
     icon.classList.remove("liked");
+    icon.classList.add("favourite-icon");
     icon.style.fill = "";
-    console.log("Icon unliked");
+
   } else {
     icon.classList.add("liked");
     icon.style.fill = "#ff0000";
-    console.log("Icon liked");
+    icon.classList.remove("favourite-icon");
   }
 });
 });
-console.log("Hello from Favourite Icon");
+
 }
