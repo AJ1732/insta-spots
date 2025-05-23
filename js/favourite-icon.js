d7b3e37f3d7c160871a1d9deb59aa058dd890ad5
Akangbe Ooreoluwa <akangbepattie@gmail.com>
2025-05-23 08:42:30 +0100

    Favourite icon can now be liked and unliked

--- js/favourite-icon.js
+++ js/favourite-icon.js
@@ -1,3 +1,25 @@
 export function favouriteIcon() {
-  console.log("Hello from Favourite Icon");
+
+const favouriteIcons = document.querySelectorAll("#favourite-icon");
+
+console.log("Found icons:", favouriteIcons);
+console.log("Number of icons:", favouriteIcons.length);
+
+favouriteIcons.forEach(icon => {
+  console.log("Adding event listener to icon:", icon);
+  icon.addEventListener("click", () => {
+    console.log("Icon clicked:", icon);
+  
+  if (icon.classList.contains("liked")) {
+    icon.classList.remove("liked");
+    icon.style.fill = "";
+    console.log("Icon unliked");
+  } else {
+    icon.classList.add("liked");
+    icon.style.fill = "#ff0000";
+    console.log("Icon liked");
+  }
+});
+});
+console.log("Hello from Favourite Icon");
 }
